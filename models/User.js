ca92e457bd87c384c6b5b3b1b0568f8ed249e8e1
Shane Moyo <shanemoyo@Boulders-MacBook-Pro.local>
2021-01-20 15:52:33 -0800

    Create User model

---
+++ models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const requiredString = {
+    type: String,
+    required: true
+  };
+
+
+const UserSchema = new mongoose.Schema({
+  name: requiredString,
+  email: requiredString,
+  password: requiredString,
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+module.exports = User = mongoose.model("users", UserSchema);
